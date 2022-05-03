0883e8d4547dd82335d4263d6ab421813edc1c89
David Perez <davidpp124@gmail.com>
2022-05-02 20:30:31 -0500

    Manual fix to prevent using private repo

--- git-ui-tool.rb
+++ git-ui-tool.rb
@@ -13,7 +13,7 @@ class GitUiTool < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/DavidPerezP124/git-tool/releases/download/v0.0.1/git-tool_0.0.1_darwin_arm64.tar.gz", :using => CurlDownloadStrategy
+      url "https://github.com/DavidPerezP124/homebrew-git-ui-tool/releases/download/v0.0.1/git-tool_0.0.1_darwin_arm64.tar.gz", :using => CurlDownloadStrategy
       sha256 "27a4c8956b4a0043bb62cd585c6ac3f654c09a9d1183755d8b299ab4b566f2b6"
 
       def install
@@ -21,7 +21,7 @@ class GitUiTool < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/DavidPerezP124/git-tool/releases/download/v0.0.1/git-tool_0.0.1_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
+      url "https://github.com/DavidPerezP124/homebrew-git-ui-tool/releases/download/v0.0.1/git-tool_0.0.1_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
       sha256 "340d9881a3de498d6291607edfac1f8e57cf1cf001dfa71893c948886ea17cf6"
 
       def install
@@ -32,7 +32,7 @@ class GitUiTool < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/DavidPerezP124/git-tool/releases/download/v0.0.1/git-tool_0.0.1_linux_arm64.tar.gz", :using => CurlDownloadStrategy
+      url "https://github.com/DavidPerezP124/homebrew-git-ui-tool/releases/download/v0.0.1/git-tool_0.0.1_linux_arm64.tar.gz", :using => CurlDownloadStrategy
       sha256 "12b883e5f8e0c9263f20c479db32f2b84b9c59f291ed005c86bd5f057d6f9fb1"
 
       def install
@@ -40,7 +40,7 @@ class GitUiTool < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/DavidPerezP124/git-tool/releases/download/v0.0.1/git-tool_0.0.1_linux_amd64.tar.gz", :using => CurlDownloadStrategy
+      url "https://github.com/DavidPerezP124/homebrew-git-ui-tool/releases/download/v0.0.1/git-tool_0.0.1_linux_amd64.tar.gz", :using => CurlDownloadStrategy
       sha256 "b50443e5968dddf769efc2470df25cb1fcbbb4aa6ae0603ca7583b4b2f8f4705"
 
       def install
